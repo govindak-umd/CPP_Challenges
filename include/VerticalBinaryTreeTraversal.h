1e64e943430d9f529a630a7a4f9fa26c9dbedaa4
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-30 19:05:38 -0400

    Question added to header file

--- include/VerticalBinaryTreeTraversal.h
+++ include/VerticalBinaryTreeTraversal.h
@@ -1,6 +1,10 @@
 #pragma once
 #include"BinaryTreeNode.h"
 
+/**
+ * Given a binary tree, perform vertical traversal on it. In a vertical traversal, nodes of binary tree are printed
+ * in vertical order. Assume that the left and right child makes a 45-degree angle with the parent node.
+ */
 class VerticalBinaryTreeTraversal{
 
 public:
