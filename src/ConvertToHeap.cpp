abd90d3377c5f94bc9ed641b93525fb11e3558a7
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-03 16:16:28 -0400

    Starting index corrected

--- src/ConvertToHeap.cpp
+++ src/ConvertToHeap.cpp
@@ -3,13 +3,13 @@
 
 void ConvertToHeap::Heapify(int i){
 
+    int largest = i;
 
 }
 
 void ConvertToHeap::arr2Heap(){
-    int start_idx = (len_array/2) - 1;
 
-    for(int i = start_idx; i > 0; i--){
+    for(int i = len_array; i > 0; i--){
         Heapify(i);
     }
 }
