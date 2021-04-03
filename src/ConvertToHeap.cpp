f47977772766e8da97f9f82eee9ff14a34806c4a
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-03 16:33:40 -0400

    Initial Heap done

--- src/ConvertToHeap.cpp
+++ src/ConvertToHeap.cpp
@@ -26,9 +26,8 @@ void ConvertToHeap::Heapify(int i){
 }
 
 void ConvertToHeap::arr2Heap(){
-    int start_idx = (len_array/2) - 1;
 
-    for(int i = start_idx; i >= 0; i--){
+    for(int i = len_array; i >= 0; i--){
         Heapify(i);
         printArray();
     }
