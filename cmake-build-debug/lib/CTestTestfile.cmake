5a0e9f2b22a9e769e3c49a462164d9a4343d43e4
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-15 07:39:29 -0400

    Google Test lib added and CMakeLists.txt updated

---
+++ cmake-build-debug/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/govin/Desktop/CPP_Challenges/lib
+# Build directory: C:/Users/govin/Desktop/CPP_Challenges/cmake-build-debug/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/govin/Desktop/CPP_Challenges/lib/googlemock
+# Build directory: C:/Users/govin/Desktop/CPP_Challenges/cmake-build-debug/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
